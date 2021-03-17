4c69ee23ec6af1501440bf9e4049b0061a1ab868
ghabxph <11975597+ghabxph@users.noreply.github.com>
2021-03-18 00:24:11 +0800

    feat: 0.0.1 - Initial version, feature still incomplete. Not yet ready for use

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
-## 1.0.0
+## 0.0.1
 
-- Initial version, created by Stagehand
+- Principals(...).sgl(...) has been implemented. .satisfies() method was not implemented
+  yet.
+- There's still no proper documentation for this version.
+- Most importantly, this is not yet ready for use!
--- test/sgl_test.dart
+++ test/sgl_test.dart
@@ -1,101 +1,29 @@
 import 'package:test/test.dart';
 import 'package:sgl/sgl.dart';
 
+void testSgl(String description, Principals principals, Map rule, int count, List<Map> matchingPrincipals) {
+  test(description, () {
+      var i = 0;
+      var sgl = principals.sgl(rule);
+      var iterator = sgl.qualifyingPrincipals.iterator;
+      expect(iterator.count, equals(count));
+      while(iterator.moveNext()) {
+        expect(iterator.current.map, equals(matchingPrincipals[i++]));
+      }
+  });
+}
+
 void main() {
 
-  test('Test peer did authorization', () {
-      var did = didAuthorizationSection();
-      var p = Principals(did['authorization']['profiles']).transformKeyTo('key', 'id');
-      var rule1 = p.sgl(did['authorization']['rules'][0]);
-      var rule2 = p.sgl(did['authorization']['rules'][1]);
-      var rule3 = p.sgl(did['authorization']['rules'][2]);
-      var rule4 = p.sgl(did['authorization']['rules'][3]);
-  });
-/*
-  test('????', () {
-      var p = Principals(<Principal>[
-          Principal({'key': '#Mv6gmMNa', 'roles': ['edge']}),
-          Principal({'key': '#izfrNTmQ', 'roles': ['edge', 'biometric']}),
-          Principal({'key': '#02b97c30', 'roles': ['cloud']}),
-          Principal({'key': '#H3C2AVvL', 'roles': ['offline']})
-      ]).transformKeyTo('key', 'id');
-      var registerSgl = p.sgl({
-          'grant': ['register'],
-          'when': {'id': '#Mv6gmMNa'},
-          'id': '7ac4c6be'
-      });
-      var authcryptSgl = p.sgl({
-          'grant': ['route', 'authcrypt'],
-          'when': {'roles': 'cloud'},
-          'id': '98c2c9cc'
-      });
-      var edgeSgl = p.sgl({
-          'grant': ['authcrypt', 'plaintext', 'sign'],
-          'when': {'roles': 'edge'},
-          'id': 'e1e7d7bc'
-      });
-      var adminSgl = p.sgl({
-          'grant': ['key_admin', 'se_admin', 'rule_admin'],
-          'when': {
-            'any': [{'roles': 'offline'}, {'roles': 'biometric'}],
-            'n': 2
-          },
-          'id': '8586d26c'
-      });
-      var edgebioSgl = p.sgl({
-          'grant': [],
-          'when': {
-            'all':[{'roles': 'edge'}, {'roles': 'biometric'}]
-          }
-      });
+  var did = didAuthorizationSection();
+  var p = Principals(did['authorization']['profiles']).transformKeyTo('key', 'id');
+  var rules = did['authorization']['rules'];
+  var profiles = did['authorization']['profiles'];
 
-      var registerIterator = registerSgl.qualifyingPrincipals.iterator;
-      print(registerIterator.count);
-      while(registerIterator.moveNext()) {
-        print(registerIterator.current.map);
-      }
-
-      var authcryptIterator = authcryptSgl.qualifyingPrincipals.iterator;
-      print(authcryptIterator.count);
-      while(authcryptIterator.moveNext()) {
-        print(authcryptIterator.current.map);
-      }
-
-      var edgeIterator = edgeSgl.qualifyingPrincipals.iterator;
-      print(edgeIterator.count);
-      while(edgeIterator.moveNext()) {
-        print(edgeIterator.current.map);
-      }
-
-      var adminIterator = adminSgl.qualifyingPrincipals.iterator;
-      print(adminIterator.count);
-      while(adminIterator.moveNext()) {
-        print(adminIterator.current.map);
-      }
-
-      var edgebioIterator = edgebioSgl.qualifyingPrincipals.iterator;
-      print(edgebioIterator.count);
-      while(edgebioIterator.moveNext()) {
-        print(edgebioIterator.current.map);
-      }
-
-
-      //var string = 'foo,bar,baz';
-      //expect(string.split(','), equals(['foo', 'bar', 'baz']));
-  });
-
-  test('??????', () {
-      Principals(<Principal>[
-          Principal({'key': '#Mv6gmMNa', 'roles': ['edge']}),
-          Principal({'key': '#izfrNTmQ', 'roles': ['edge', 'biometric']}),
-          Principal({'key': '#02b97c30', 'roles': ['cloud']}),
-          Principal({'key': '#H3C2AVvL', 'roles': ['offline']})
-      ]).transformKeyTo('key', 'id').sgl({
-          'grant': ['register'],
-          'when': {'id': '#Mv6gmMNa'},
-          'id': '7ac4c6be'
-      });
-  }); */
+  testSgl('Testing register rule', p, rules[0], 1, [profiles[0]]);
+  testSgl('Testing cloud rule', p, rules[1], 1, [profiles[2]]);
+  testSgl('Testing edge rule', p, rules[2], 2, [profiles[0], profiles[1]]);
+  testSgl('Testing super admin rule', p, rules[3], 2, [profiles[1], profiles[3]]);
 }
 
 Map didAuthorizationSection() {
